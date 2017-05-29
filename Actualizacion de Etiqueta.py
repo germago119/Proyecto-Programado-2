c92c747fa2cefe46e5c7aaafdcb31f6a1c2d8b41
geniusg1205 <geniusg1205@gmail.com>
2017-05-28 22:48:43 -0600

    Actualizacion de una etiqueta
    Se le hace la actualizacion a una etiqueta cuando se ejecuta un boton.

---
+++ Actualizacion de Etiqueta.py
@@ -0,0 +1,23 @@
+import serial
+import time
+from tkinter import *
+
+ser = serial.Serial('COM4', 9600)
+
+root = Tk()
+root.title("Azrael")
+root.minsize(600, 600)
+root.resizable(width=NO, height=NO)
+
+lbl = Label(text = "Hola Probando el arduino").place(x = 300, y = 300)
+
+
+
+while 1:
+    if ser.readline() == b'IZQ\r\n':
+        lbl.config(text ="Mae actualizacion de ventana completa").place(x=300, y = 300)
+        root.update()
+    else:
+        lbl =  Label(text = ser.readline()).place(x = 300, y = 300)
+        print(ser.readline())
+        root.update()
