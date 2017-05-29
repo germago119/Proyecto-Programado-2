39f5b8d0937003586de813f82b85756271420b3b
Róger Valderrama <roger.andres18@gmail.com>
2017-05-29 16:13:08 -0600

    ...

--- Actualizacion de Etiqueta.py
+++ Actualizacion de Etiqueta.py
@@ -15,7 +15,7 @@ lbl = Label(text = "Hola Probando el arduino").place(x = 300, y = 300)
 while 1:
     if ser.readline() == b'IZQ\r\n':
         lbl=Label(text ="Mae actualizacion de ventana completa").place(x=300, y = 300)
-        root.after(100)
+        root.update()
     else:
         lbl =  Label(text = "Mae no estoy recibiendo nada jajaj").place(x = 300, y = 300)
-        root.after(100)
+        root.update()
