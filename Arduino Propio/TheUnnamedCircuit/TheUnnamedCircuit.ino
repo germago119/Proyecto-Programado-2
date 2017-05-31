96b3ceea6a5d63db6d56d49f1e72abee0be4f0f6
geniusg1205 <geniusg1205@gmail.com>
2017-05-30 21:56:33 -0600

    Actualización de Arduino

--- Arduino Propio/TheUnnamedCircuit/TheUnnamedCircuit.ino
+++ Arduino Propio/TheUnnamedCircuit/TheUnnamedCircuit.ino
@@ -13,6 +13,7 @@ int boton1 = 13;
 int boton2 = 12;  //izq
 int boton3 = 11;
 int boton4 = 10;//evitar usar pines 0 y 1
+int boton5 = 9;
 void setup() {
   //inicializa la comunicacion serial
   Serial.begin(9600); //9600 es la "velocidad", el mismo valor debe ser seleccionado en el monitor serial
@@ -20,6 +21,7 @@ void setup() {
   pinMode(boton2, INPUT_PULLUP);
   pinMode(boton3, INPUT_PULLUP);
   pinMode(boton4, INPUT_PULLUP);
+  pinMode(boton5, INPUT_PULLUP);
   //declaramos el pin como una entrada digital (HIGH o LOW, 0 o 5V)
 }
 
@@ -27,6 +29,7 @@ void loop() {
   int button1State = digitalRead(boton1);
   int button2State = digitalRead(boton2);
   int button3State = digitalRead(boton3);
+  int button4State = digitalRead(boton4);
   int button4State = digitalRead(boton4);
     //lee el estado del pin (0 o 1, 0 o 5v)
   if (button1State == 0)
@@ -44,6 +47,10 @@ void loop() {
   if (button4State == 0)
   {
     Serial.println("OWN");
+  }
+  if (button4State == 0)
+  { 
+    Serial.println("PRESENTATION");
   }
 delay(10);        // espera para la siguiente lectura
 }
